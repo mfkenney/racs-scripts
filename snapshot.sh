56de865b366553600b8051da7b59d44d150d9f7d
Michael Kenney <mfkenney@gmail.com>
2015-01-08 15:14:07 -0800

    Add a verbose option to the snapshot script.

--- snapshot.sh
+++ snapshot.sh
@@ -14,8 +14,24 @@ RACS_STREAM_TIME=1
 
 [ -e $CFGDIR/settings ] && . $CFGDIR/settings
 
+status ()
+{
+    msg="$1"
+    pct="$2"
+    logger -p "local0.info" "$msg"
+    if [ -n "$pct" ]
+    then
+        echo "$pct"
+        echo "XXX"
+        echo "$msg"
+        echo "XXX"
+    fi
+}
+
 camera="$1"
 [ -z "$camera" ] && exit 1
+
+verbose="$2"
 
 # Full pathname for the snapshot file
 img="$SNAPSHOTDIR/${camera}.jpg"
@@ -24,7 +40,7 @@ img="$SNAPSHOTDIR/${camera}.jpg"
 # which will result in an erroneous EXIF timestamp.
 rm -f $img
 
-logger -p "local0.info" "Taking a snapshot from $camera"
+status "Taking a snapshot from $camera" ${verbose:+0}
 cvlc -I dummy -q --run-time=$RACS_STREAM_TIME \
      "http://${camera}/nph-mjpeg.cgi?0" \
      --vout=dummy \
@@ -39,22 +55,25 @@ if [ -e "$img" ]
 then
     base="$(basename $img)"
     # Add EXIF date/time to the image file
-    jhead -mkexif -dsft $img
+    status "Adding EXIF header" ${verbose:+50}
+    jhead -mkexif -dsft $img 1>&2
     # Rescale for upload
-    djpeg "$img" | pnmscale $RACS_SCALE | cjpeg > $OUTBOX/$base
+    status "Rescaling image" ${verbose:+55}
+    djpeg "$img" | pnmscale $RACS_SCALE | cjpeg > $OUTBOX/$base 1>&2
     # Transfer EXIF header to the scaled image
-    jhead -te $img $OUTBOX/$base
+    jhead -te $img $OUTBOX/$base 1>&2
     # Archive the snapshot using a date/time based directory
     # scheme.
+    status "Archiving original image" ${verbose:+95}
     here=$(pwd)
     cd $SNAPSHOTDIR
-    jhead -nf$ARCHIVEDIR/%f/%Y/%m/%d/%f_%Y%m%d_%H%M%S $base
+    jhead -nf$ARCHIVEDIR/%f/%Y/%m/%d/%f_%Y%m%d_%H%M%S $base 1>&2
     cd $here
     # Rename the scaled image to match
     cd $OUTBOX
-    jhead -nf%f_%Y%m%d_%H%M%S $base
+    jhead -nf%f_%Y%m%d_%H%M%S $base 1>&2
     cd $here
-    logger "Archived snapshot from $camera"
+    status "Archived snapshot from $camera" ${verbose:+100}
 else
     logger -p "local0.warning" "Snapshot failed ($camera)"
     exit 2
