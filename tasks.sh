c30f272c2ac89aaeca8f07e0bca78b55b6aa95bc
Michael Kenney <mfkenney@gmail.com>
2015-01-15 13:06:30 -0800

    Major updates to the tasks script.
    * General code formatting fixes.
    * Sort the files to be uploaded in date/time order.
    * Set a time limit on the file upload time.

--- tasks.sh
+++ tasks.sh
@@ -11,16 +11,16 @@ camera_up ()
 
 power_on ()
 {
-    for arg
-    do
+    local arg
+    for arg; do
         tsctl @localhost DIO setasync "$arg" HIGH
     done
 }
 
 power_off ()
 {
-    for arg
-    do
+    local arg
+    for arg; do
         tsctl @localhost DIO setasync "$arg" LOW
     done
 }
@@ -33,17 +33,16 @@ ID="$(hostname -s)"
 [ -e $CFGDIR/settings ] && . $CFGDIR/settings
 [ -e $HOME/bin/library.sh ] && . $HOME/bin/library.sh
 
-if [ -e /tmp/INHIBIT ]
-then
-    log_event "INFO" "Autonomous operation inhibited"
+if [ -e /tmp/INHIBIT ]; then
+    logger -p "local0.info" "Autonomous operation inhibited"
     exit 1
 fi
 
 # Remove old files from the OUTBOX
 clean_dir "$OUTBOX" "$RACS_MAX_AGE"
 
-# Start the A/D monitor
-# TODO: add A/D config file
+# Start the A/D monitor and store the output in the OUTBOX.
+# FIXME: add A/D config file
 adread --interval=5s > $OUTBOX/adc.csv &
 child=$!
 
@@ -56,25 +55,21 @@ power_on "${RACS_CAMERA_POWER[@]}"
 # Wait for all cameras to boot
 up=()
 n_up=0
-for ((i = 1; i <= RACS_NCAMERAS; i++))
-do
+for ((i = 1; i <= RACS_NCAMERAS; i++)); do
     name="camera-${i}"
-    if wait_for_camera $name $RACS_CAMERA_BOOTTIME
-    then
+    if wait_for_camera $name $RACS_CAMERA_BOOTTIME; then
         up+=("$name")
         ((n_up++))
     fi
 done
 
-if ((n_up == 0))
-then
+if ((n_up == 0)); then
     log_event "ERROR" "No cameras available!"
 else
     # Allow additional warm-up time
     sleep $RACS_CAMERA_WARMUP
     # Take a snapshot from each one and power it off
-    for c in "${up[@]}"
-    do
+    for c in "${up[@]}"; do
         idx=$(cut -f2 -d- <<< "$c")
         i=$((idx - 1))
         snapshot.sh "$c" "${RACS_CAMERA_POWER[i]}"
@@ -86,8 +81,7 @@ power_off "${RACS_CAMERA_POWER[@]}"
 
 # If we are in autonomous mode, it's safe to power off
 # the ethernet switch.
-if [ -z "$RACS_NOSLEEP" ]
-then
+if [ -z "$RACS_NOSLEEP" ]; then
     power_off "$RACS_ENET_POWER"
     log_event "INFO" "Ethernet switch powered off"
 fi
@@ -99,8 +93,7 @@ log_event "INFO" "Initiating PPP link"
 
 # Download configuration updates and the list of
 # requested full-res images to the INBOX
-if [ -n "$RACS_FTP_SERVER" ]
-then
+if [ -n "$RACS_FTP_SERVER" ]; then
     ftp -p $RACS_FTP_SERVER<<EOF
 cd outgoing/$ID
 lcd $INBOX
@@ -109,22 +102,22 @@ delete updates
 get fullres.txt
 delete fullres.txt
 EOF
-    [ -e "$INBOX/updates" ] && mv "$INBOX/updates" "$CFGDIR"
+    if [ -e "$INBOX/updates" ]; then
+        mv "$INBOX/updates" "$CFGDIR"
+        . $CFGDIR/settings
+    fi
+
+    # Locate full-res images and add to OUTBOX
+    if [ -e "$INBOX/fullres.txt" ]; then
+        while read name; do
+            findimg.sh "$name"
+        done <"$INBOX/fullres.txt"
+        rm -f "$INBOX/fullres.txt"
+    fi
 fi
 
-# Locate full-res images and add to OUTBOX
-if [ -e "$INBOX/fullres.txt" ]
-then
-    while read name
-    do
-        findimg.sh "$name"
-    done <"$INBOX/fullres.txt"
-    rm -f "$INBOX/fullres.txt"
-fi
-
-# Stop the A/D monitor and save the output to the OUTBOX
-if [ -n "$child" ]
-then
+# Stop the A/D monitor
+if [ -n "$child" ]; then
     kill -TERM $child
     wait $child
 fi
@@ -134,19 +127,41 @@ sudo ntpdate -b $RACS_NTP_SERVER 1> $OUTBOX/ntp.out 2>&1
 
 # Upload files from the OUTBOX. Files are removed after
 # they are successfully transfered.
-if [ -n "$RACS_FTP_SERVER" ]
-then
+#
+if [ -n "$RACS_FTP_SERVER" ]; then
     (
+        filelist="/tmp/uploads"
+        sort_arg="${RACS_REV_SORT:+-r}"
         cd $OUTBOX
+        # Archive all of the non-image files
         zip_non_jpeg
-        wput -nv --disable-tls -B -R * ftp://$RACS_FTP_SERVER/incoming/$ID/
+
+        # Sort files in timestamp order, oldest first by default. The
+        # creation timestamp is incorporated into the filename so we
+        # use that rather than the filesystem time.
+        for f in *; do
+            t=$(cut -f2-3 -d_ <<< "${f%.*}")
+            [[ $t ]] && echo "$t $f"
+        done | sort $sort_arg | cut -f2- -d' ' > $filelist
+
+        # Start the file upload
+        wput -nv --tries=1 \
+             --disable-tls -B -R -i $filelist \
+             ftp://$RACS_FTP_SERVER/incoming/$ID/ &
+
+        # Set a time limit for the file transfer to complete
+        child=$!
+        n=$RACS_FTP_TIMELIMIT
+        while sleep 1; do
+            kill -0 $child || break
+            if ((--n <= 0)); then
+                kill $child
+                break
+            fi
+        done
+        wait $child
     )
 fi
 
 # Shutdown until next sample time
-if [ -n "$RACS_NOSLEEP" ]
-then
-    :
-else
-    set_alarm.sh $RACS_INTERVAL
-fi
+[ -z "$RACS_NOSLEEP" ] && set_alarm.sh $RACS_INTERVAL
