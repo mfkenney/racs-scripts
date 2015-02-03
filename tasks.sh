3ab6e3fe7916f4960b11967b9ad35f7d0809fbbb
Michael Kenney <mfkenney@gmail.com>
2015-02-03 12:52:46 -0800

    New timeout handling scheme.
    Added a global timeout for the entire PPP link time
    rather than the individual FTP connections.

--- tasks.sh
+++ tasks.sh
@@ -4,27 +4,6 @@
 #
 export PATH=$HOME/bin:/usr/local/bin:$PATH
 
-camera_up ()
-{
-    curl --connect-timeout 2 -s -X HEAD http://$1/index.html > /dev/null
-}
-
-power_on ()
-{
-    local arg
-    for arg; do
-        tsctl @localhost DIO setasync "$arg" HIGH
-    done
-}
-
-power_off ()
-{
-    local arg
-    for arg; do
-        tsctl @localhost DIO setasync "$arg" LOW
-    done
-}
-
 CFGDIR="$HOME/config"
 OUTBOX="$HOME/OUTBOX"
 INBOX="$HOME/INBOX"
@@ -33,8 +12,19 @@ ID="$(hostname -s)"
 [[ -e $CFGDIR/settings ]] && . $CFGDIR/settings
 [[ -e $HOME/bin/library.sh ]] && . $HOME/bin/library.sh
 
+cleanup_and_shutdown ()
+{
+    logger -s -p "local0.info" "Shutting down"
+    # Bring down PPP link and power-down the modem
+    sudo poff iridium
+    power_off "$RACS_MODEM_POWER"
+
+    # Shutdown until next sample time
+    [[ "$RACS_NOSLEEP" ]] || set_alarm.sh $RACS_INTERVAL
+}
+
 if [[ -e /tmp/INHIBIT ]]; then
-    logger -p "local0.info" "Autonomous operation inhibited"
+    logger -s -p "local0.info" "Autonomous operation inhibited"
     exit 1
 fi
 
@@ -94,12 +84,12 @@ log_event "INFO" "Initiating PPP link"
 sudo pon iridium persist
 
 # Wait for link to be established
-if ppp_wait $RACS_PPP_TIMELIMIT; then
+if ppp_wait $RACS_PPP_LINKTIME; then
     log_event "INFO" "PPP link up"
     sudo ip route add $RACS_FTP_SERVER/32 dev ppp0
     sudo ip route add $RACS_NTP_SERVER/32 dev ppp0
 else
-    logger -p "local0.emerg" "Cannot establish PPP link"
+    logger -s -p "local0.emerg" "Cannot establish PPP link"
     # If the link cannot be established, power off the
     # modem and bailout. If we are in autonomous mode,
     # shutdown until the next interval.
@@ -108,6 +98,10 @@ else
     [[ "$RACS_NOSLEEP" ]] && exit 1
     set_alarm.sh $RACS_INTERVAL
 fi
+
+# Set a time limit for the rest of the script to finish
+trap cleanup_and_shutdown ALRM
+sleep $RACS_PPP_TIMELIMIT && kill -ALRM $$ &
 
 # Download configuration updates and the list of
 # requested full-res images to the INBOX
@@ -168,23 +162,12 @@ if [[ "$RACS_FTP_SERVER" ]]; then
              --disable-tls -B -R -i $filelist \
              ftp://$RACS_FTP_SERVER/incoming/$ID/ &
 
-        # Set a time limit for the file transfer to complete
+        # Wait for the file transfer to complete. Running wput
+        # asynchronously allows us to be interrupted by the
+        # PPP_TIMELIMIT alarm immediately.
         child=$!
-        n=$RACS_FTP_TIMELIMIT
-        while sleep 1; do
-            kill -0 $child 2> /dev/null || break
-            if ((--n <= 0)); then
-                kill $child
-                break
-            fi
-        done
         wait $child
     )
 fi
 
-# Bring down PPP link and power-down the modem
-sudo poff iridium
-power_off "$RACS_MODEM_POWER"
-
-# Shutdown until next sample time
-[[ "$RACS_NOSLEEP" ]] || set_alarm.sh $RACS_INTERVAL
+cleanup_and_shutdown
